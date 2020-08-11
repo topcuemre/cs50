3523677124f7047be4a9aca934f3693f1015523d
topcuemre <topcuemre@users.noreply.github.com>
2020-08-11 19:43:02 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
@@ -11,40 +12,34 @@ int main(int argc, string argv[])
     }
     else
     {
+        for (int i = 0; i < strlen(argv[1]) ; i++)
+        {
+            if (!(argv[1][i] <= '9' && argv[1][i] >= '0'))
+            {
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
+        }
         printf("Success\n");
     }
-    for (int i = 0; i < strlen(argv[1]) ; i++)
+    string k = get_string("plaintext:");
+    int a = strlen(k);
+    int val = atoi(argv[1]);
+    for (int i = 0; i < a; i++ )
     {
-        if (!(argv[1][i] <= '9' && argv[1][i] >= '0'))
+        if (k[i] >= 65 && k[i] <= 90)
         {
-            printf("Usage: ./caesar key");
-            return 1;
+            k[i] = ((int)k[i] + val);
+        }
+        
+        else if (k[i] >= 97 && k[i] <= 122)
+        {               
+            k[i] = ((int)k[i] + val);
         }
         else
         {
-            break;
+            continue;
         }
     }
-    int cntr = (int)argv[1];
-        string k = get_string("plaintext:");
-        for (int i = 0; i < strlen(k); i++ )
-        {
-            if (k[i] >= 65 && k[i] <= 90)
-            {
-                k[i] = (k[i] + cntr) % 26;
-            }
-            
-            else if (k[i] >= 97 && k[i] <= 122)
-            {
-                k[i] = (k[i] + cntr) % 26;
-            }
-            else
-            {
-                continue;
-            }
-            
-        }
-        
-        printf("ciphertext: %s\n",k);
-    
+    printf("ciphertext:%s\n",k);
 }
