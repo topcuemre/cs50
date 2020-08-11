94315f64f427dd99e2215396657e1d0ebf392701
topcuemre <topcuemre@users.noreply.github.com>
2020-08-11 09:05:28 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -4,31 +4,32 @@
 int main(void)
 {
     int cntr = 0;
-    int height;
-    int n;
-    
-    while (!(height>=1 && height<=8))
+    int n, height;
+    do
     {
-        int height = get_int("Height:");
-        if (height>=1 && height<=8)
+        int height= get_int("Height:\n");
+        if(height>=1 && height <=8)
         {
-        printf("Stored: %i\n",height);
-        int n = height;
-         for (int i=0;i<height;i++)
+            printf("Stored : %i\n",height);
+            int n = height;
+             for (int i=0;i<height;i++)
+    {
+        for (int j=1;j<n;j++)
             {
-                for (int j=1;j<n;j++)
-                    {
-                        printf(" ");
-                    }
-                cntr++;
-                for (int k=1;k<=cntr;k++)
-                    {
-                        printf("#");
-                    }
-                n--;
-                printf("\n");
+                printf(" ");
             }
-        }
-        
+        cntr++;
+        for (int k=1;k<=cntr;k++)
+            {
+                printf("#");
+            }
+        n--;
+        printf("\n");
     }
+    break;
+        };
+    } while(!(height>=1 && height <=8));
+    
+  
+   
 }
