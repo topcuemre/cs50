fd0968da5f5596b20c6d4dcb5fad8f8fa6d697b9
topcuemre <topcuemre@users.noreply.github.com>
2020-08-11 12:48:45 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main (void)
 {
     /* asks user the dollar value */
-    int counter = 0;
     float dollars;
-    
-    float cents = get_float("Change owed:\n") * 100.0 ;
-    
-    // main idea of greedy algorithms that in cashier problem you have to get rid of the largest cents before the smaller ones
-    while ( cents > 0. )
+    do
     {
-        while (cents - 25.0 >= 0.0)
+        dollars = get_float("Change owed:\n");
+    }
+    while( dollars < 0);
+    int cents = round(dollars * 100);
+    int counter = 0;
+    while ( cents >  0. )
+    {
+        if (cents - 25.0 >= 0.0)
         {
             cents = cents - 25.0;
             counter++;
             
         }
         
-        while ( cents - 10.0 >= 0.0 )
+        else if ( cents - 10.0 >= 0.0 )
         {
             cents = cents - 10.0;
             counter++;
             
         }
         
-        while ( cents - 5.0 >= 0.0 )
+        else if ( cents - 5.0 >= 0.0 )
         {
             cents = cents - 5.0;
             counter++;
         }
         
-        while (cents - 1.0 >= 0.0)
+        else if (cents - 1.0 >= 0.0)
         {
             cents = cents - 1.0;
             counter++;
