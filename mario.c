7efd43ca262c082b78403b7345c7d230766d2f69
topcuemre <topcuemre@users.noreply.github.com>
2020-08-11 09:17:04 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 #include <cs50.h>
-
+//This programs creates the magnificent mountains of mario
 int main(void)
 {
     int cntr = 0;
     int n, height;
+    //in this part program asks the height of pyramid user wants to create
     do
     {
-        int height= get_int("Height:\n");
-        if(height>=1 && height <=8)
+        /* do loop is executed at least one time even if the while condition is not met */
+        int height = get_int("Height:\n");
+        if (height >= 1 && height <= 8)
         {
-            
             int n = height;
-             for (int i=0;i<height;i++)
-    {
-        for (int j=1;j<n;j++)
+            for (int i = 0 ; i < height ; i++)
             {
-                printf(" ");
+                for (int j = 1; j < n ; j++)
+                {
+                    printf(" ");
+                }
+                cntr++;
+                for (int k = 1; k <= cntr; k++)
+                {
+                    printf("#");
+                }
+                n--;
+                printf("\n");
             }
-        cntr++;
-        for (int k=1;k<=cntr;k++)
-            {
-                printf("#");
-            }
-        n--;
-        printf("\n");
-    }
-    break;
-        };
-    } while(!(height>=1 && height <=8));
-    
-  
-   
+            break;
+        };/* program prompts simultaneously until a valid input of height is given */
+    } 
+    while (!(height >= 1 && height <= 8)); 
 }
