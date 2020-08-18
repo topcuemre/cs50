a9e613b034aadb81d27b9e8e76b720da5fef4784
topcuemre <topcuemre@users.noreply.github.com>
2020-08-18 09:44:00 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,7 +1,7 @@
 from cs50 import get_int
-cond = True;
-while (cond):
-    height = get_int("Height :")
+
+while (True):
+    height = get_int("Height: ")
     if not (height <= 1 or height > 8):
         break
 
@@ -10,7 +10,7 @@ for i in range(height):
     print("#" * (i + 1),end="")
     print("  ",end="")
     print("#" * (i + 1),end="")
-    print("\n",end="")
+    print(end="\n")
 
 
 
