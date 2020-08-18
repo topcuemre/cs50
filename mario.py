c8ff3d592629ac6414f19dcdba8b5f675fe6907a
topcuemre <topcuemre@users.noreply.github.com>
2020-08-18 10:05:37 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,10 +1,12 @@
 from cs50 import get_int
-
+# this is for input the height of pyramid only int values valid
+#while loop run over and over again until a value between 0 and 8 is entered
 while (True):
     height = get_int("Height: ")
     if not (height <= 1 or height > 8):
         break
-
+#to form pyramid i used for loop
+# every print values end parameter is set to "" in order to create a good shaped pyramid
 for i in range(height):
     print(" " * (height - i - 1),end="")
     print("#" * (i + 1),end="")
